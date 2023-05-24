848baeb06c7584e8485a0bea58d86b345fe2fe4f
Indranil Halder <indranilhalder.dev@gmail.com>
2023-05-24 19:44:33 +0100

    Django Templates and dynamic values

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
+projectsList = [
+    {
+        "id": "1",
+        "title": "E-commerce Web app",
+        "description": "Fully functional e-commerce website"
+    },
+    {
+        "id": "2",
+        "title": "Portfolio Website",
+        "description": "My portfolio website"
+    },
+    {
+        "id": "3",
+        "title": "DevConnector",
+        "description": "Social Media app for developers"
+    },
+]
 
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = "projects"
+    number = 10
+    context = {'page': page, 'number': number, 'projects': projectsList}
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+
+    return render(request, 'projects/single-project.html', {'project': projectObj})
