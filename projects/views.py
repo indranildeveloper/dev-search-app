fd6cc448b28d85da0951bd784ff3588349f75918
Indranil Halder <indranilhalder.dev@gmail.com>
2023-05-24 15:43:11 +0530

    Projects urls setup

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def projects(request):
+    return HttpResponse("Here are our projects")
+
+
+def project(request, pk):
+    return HttpResponse("Here is a single project" + " " + pk)
