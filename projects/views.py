5a5fb53f45594effc87345525b81309fccf4c8dc
Indranil Halder <indranilhalder.dev@gmail.com>
2023-05-24 11:39:00 +0100

    Django Templates Setup

--- projects/views.py
+++ projects/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse("Here are our projects")
+    return render(request, 'projects/projects.html')
 
 
 def project(request, pk):
-    return HttpResponse("Here is a single project" + " " + pk)
+    return render(request, 'projects/single-project.html')
