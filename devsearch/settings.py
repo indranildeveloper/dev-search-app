10a241a86fb1c329788310dd673a2bc84570460d
Indranil Halder <indranilhalder.dev@gmail.com>
2023-05-26 22:02:59 +0100

    Static files serve STATIC_ROOT and STATIC_URL

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l%ia5^=!%(p1(d)r37q=xv67-&ot!x6=3$mo=k!28z_th&=b#g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
