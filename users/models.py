7730d7aaaa6ec025091cc0f61427e4fe06620c20
Indranil Halder <indranilhalder.dev@gmail.com>
2023-06-01 02:04:24 +0530

    Implemented Signals in Users app

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
+
 # Create your models here.
 
 
@@ -25,7 +26,7 @@ class Profile(models.Model):
                           primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
