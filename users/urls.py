79b6876463f6b5326ad0ec35fba38c036c6a6162
Indranil Halder <indranilhalder.dev@gmail.com>
2023-06-04 01:50:37 +0530

    Register User

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.registerUser, name='register'),
     path('login/', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
+
+
     path('', views.profiles, name="profiles"),
     path('profile/<str:pk>', views.userProfile, name='user-profile'),
 ]
